ec7c2ef073ce0ddb2601baa76f180681611b47fb
Sefa Esendemir <sefa.esendemir@gmail.com>
2024-11-26 13:04:55 +0300

    fix wrong property for transaction

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -28,7 +28,6 @@ export default function TransactionsPage() {
       category: transaction.category,
       description: transaction.description,
       date: transaction.date,
-      store: transaction.store
     }))
     setIsFormOpen(false)
   }
